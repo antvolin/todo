47072474811b82881030013d8e770a5ec07e77dd
antvolin <antvolin@gmail.com>
2020-01-07 19:19:26 +0200

    Added test for FilterRequestHandler

---
+++ tests/Lib/Handler/FilterRequestHandlerTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace BeeJeeMVC\Tests\Lib;
+
+use BeeJeeMVC\Lib\Factory\RequestFactory;
+use BeeJeeMVC\Lib\Handler\FilterRequestHandler;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class FilterRequestHandlerTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    protected $originalText;
+
+    /**
+     * @var Request
+     */
+    protected $request;
+
+    protected function setUp()
+    {
+        $this->originalText = '<script>alert("test");</script>';
+        $this->request = (new RequestFactory())->create();
+        $this->request->request->set('text', $this->originalText);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldBeAddingEscapingSpecialCharacters(): void
+    {
+        (new FilterRequestHandler())->handle($this->request);
+
+        $this->assertNotEquals($this->originalText, $this->request->request->get('text'));
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider specialChars
+     *
+     * @param string $char
+     */
+    public function specialCharactersFromRequestValueShouldBeConvertedToHtmlEntity(string $char): void
+    {
+        (new FilterRequestHandler())->handle($this->request);
+
+        $this->assertFalse(strpos($char, $this->request->request->get('text')));
+    }
+
+    /**
+     * @return array
+     */
+    public function specialChars(): array
+    {
+        return [
+            ['>'],
+            ['<'],
+            ['"'],
+            ['`'],
+        ];
+    }
+}
