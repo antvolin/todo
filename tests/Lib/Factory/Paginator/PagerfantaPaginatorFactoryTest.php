024cc610663a87c33b83c95e459619b2a67439e9
antvolin <antvolin@gmail.com>
2020-01-26 16:47:11 +0200

    Replaced the objects with a stubs in the test PagerfantaPaginatorFactoryTest

--- tests/Lib/Factory/Paginator/PagerfantaPaginatorFactoryTest.php
+++ tests/Lib/Factory/Paginator/PagerfantaPaginatorFactoryTest.php
@@ -15,8 +15,23 @@ class PagerfantaPaginatorFactoryTest extends TestCase
     {
         $rows = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
         $countRows = count($rows);
+        $entityPerPage = 3;
+        $currentPage = 3;
 
-        $paginator = (new PagerfantaPaginatorFactory(new PaginatorAdapter(), 3))->create($rows, $countRows, 3);
+        $adapter = $this->getMockBuilder(PaginatorAdapter::class)
+            ->setMethods(['setData', 'setCountRows', 'getNbResults'])
+            ->getMock();
+        $adapter->expects($this->once())
+            ->method('setData')
+            ->with($this->equalTo($rows));
+        $adapter->expects($this->once())
+            ->method('setCountRows')
+            ->with($this->equalTo($countRows));
+        $adapter->method('getNbResults')->willReturn($countRows);
+
+        $factory = new PagerfantaPaginatorFactory($adapter, $entityPerPage);
+
+        $paginator = $factory->create($rows, $countRows, $currentPage);
 
         $this->assertTrue(method_exists($paginator, 'getCurrentPageResults'));
         $this->assertTrue(method_exists($paginator, 'getHtml'));
